520770bb8e0c288cfcc28fc901d59b67cf22dbef
Daniel Berg <mail@roosta.sh>
2019-08-26 19:55:50 +0200

    Store query, fix jumping below window height

--- fzf-search.sh
+++ fzf-search.sh
@@ -1,22 +1,36 @@
 #!/usr/bin/env bash
 
 _fzf_cmd() {
-    fzf-tmux --delimiter=":" --ansi --with-nth="2.." --no-preview
+    fzf-tmux --delimiter=":" --ansi --with-nth="2.." --no-preview --print-query
 }
 
 _enter_mode() {
 	tmux copy-mode
 }
 
+# "manually" go up in the scrollback for a number of lines
+_manually_go_up() {
+	local line_number="$1"
+	tmux send-keys -X -N "$line_number" cursor-up
+	tmux send-keys -X start-of-line
+}
+
 main() {
-  local conten, match, line_number corrected
+  local conten, match, line_number, corrected, window_height
   content="$(tmux capture-pane -e -J -p -S -)"
   match=$(echo "$content" | tac | nl -b 'a' -s ':' | _fzf_cmd)
-  line_number=$(echo "$match" | cut -d':' -f1 | tr -d '[:space:]')
+  query=$(echo "$match" | cut -d$'\n' -f1)
+  rest=$(echo "$match" | cut -d$'\n' -f2)
+  line_number=$(echo "$rest" | cut -d':' -f1 | tr -d '[:space:]')
   corrected=$((line_number - 1))
+	window_height="$(tmux display-message -p '#{pane_height}')"
 
   _enter_mode
-  tmux send-keys -X goto-line "$corrected"
+  if [ "$corrected" -lt "$window_height" ]; then
+    _manually_go_up "$corrected"
+  else
+    tmux send-keys -X goto-line "$corrected"
+  fi
 }
 
 main
