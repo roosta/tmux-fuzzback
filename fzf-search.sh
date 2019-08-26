bb0c55e974dedc9726ef38572d3ed96e47dd5585
Daniel Berg <mail@roosta.sh>
2019-08-26 18:57:13 +0200

    Grab line number

--- fzf-search.sh
+++ fzf-search.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 fzf_cmd() {
-    fzf-tmux --cycle --bind='ctrl-s:toggle-sort' --no-preview
+    fzf-tmux --delimiter=":" --with-nth="2.." --ansi --no-preview
 }
 
-content="$(tmux capture-pane -J -p -S -)"
-echo "$content"
+content="$(tmux capture-pane -e -J -p -S -)"
+match=$(echo "$content" | tac | nl -b 'a' -s ':' | fzf_cmd)
+line_nr=$(echo "$match" | cut -d':' -f1 | tr -d '[:space:]')
+echo "$line_nr"
+# echo $match
+# line_nr=()
