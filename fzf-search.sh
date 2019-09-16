7d09044f3b81cc9cb8a237ccf1d2419f99e62377
Daniel Berg <mail@roosta.sh>
2019-09-16 19:30:06 +0200

    Add match line position and jump

--- fzf-search.sh
+++ fzf-search.sh
@@ -25,6 +25,36 @@ _manually_go_up() {
 	tmux send-keys -X start-of-line
 }
 
+
+_escape_backslash() {
+	local string="$1"
+	echo "$(echo "$string" | sed 's/\\/\\\\/g')"
+}
+
+_get_match_line_position() {
+	local query="$1"
+  local result_line="$2"
+	local platform index zero_index
+
+	# OS X awk cannot have `=` as the first char in the variable (bug in awk).
+	# If exists, changing the `=` character with `.` to avoid error.
+	platform="$(uname)"
+	if [ "$platform" == "Darwin" ]; then
+		result_line="$(echo "$result_line" | sed 's/^=/./')"
+		query="$(echo "$query" | sed 's/^=/./')"
+	fi
+
+	# awk treats \r, \n, \t etc as single characters and that messes up match
+	# highlighting. For that reason, we're escaping backslashes so above chars
+	# are treated literally.
+	result_line="$(_escape_backslash "$result_line")"
+	query="$(_escape_backslash "$query")"
+
+	index=$(awk -v a="$result_line" -v b="$query" 'BEGIN{print index(a,b)}')
+	zero_index=$((index - 1))
+	echo "$zero_index"
+}
+
 # maximum line number that can be reached via tmux 'jump'
 # https://github.com/tmux-plugins/tmux-copycat/blob/e95528ebaeb6300d8620c8748a686b786056f374/scripts/copycat_jump.sh#L159
 _get_max_jump() {
@@ -79,19 +109,27 @@ _get_line_number() {
 }
 
 main() {
-  local content match line_number window_height query max_lines max_jump correction correct_line_number
+
+  local content match line_number window_height query max_lines
+  max_jump correction correct_line_number trimmed_line match_line_position
+
   content="$(tmux capture-pane -e -J -p -S -)"
   match=$(echo "$content" | tac | nl -b 'a' -s ':' | _fzf_cmd)
   query=$(echo "$match" | cut -d$'\n' -f1)
   rest=$(echo "$match" | cut -d$'\n' -f2)
+  trimmed_line=$(echo "$rest" | sed 's/[[:space:]]\+[[:digit:]]\+://')
   line_number=$(_get_line_number "$rest")
   window_height="$(tmux display-message -p '#{pane_height}')"
   max_lines=$(echo "$content" | wc -l)
   max_jump=$(_get_max_jump "$max_lines" "$window_height")
   correction="0"
+  match_line_position=$(_get_match_line_position "$query" "$trimmed_line")
 
   if [ -n "$match" ]; then
 
+    # echo $query
+    # echo $trimmed_line
+    # echo $match_line_position
 
     if [ "$line_number" -gt "$max_jump" ]; then
       # We need to 'reach' a line number that is not accessible via 'jump'.
@@ -116,6 +154,10 @@ main() {
       local half_window_height="$((window_height / 2))"
       # creating as much padding as possible, up to half pane height
       _create_padding_below_result "$line_number" "$half_window_height"
+    fi
+
+    if [ "$match_line_position" -gt "0" ]; then
+      tmux send-keys -X -N "$match_line_position" cursor-right
     fi
 
   fi
