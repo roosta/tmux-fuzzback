7948ba421a942243d9a0c91c670f555ad16a08de
Daniel Berg <mail@roosta.sh>
2021-03-04 17:57:00 +0100

    fix: movement when in a window (vim, ssh etc)
    This is a combination of 21 commits:
    
    - WIP
    - WIP: use filenames, refactor entire main fn
    - WIP
    - WIP: re-enable horizontal movement
    - WIP: fix upward movement
       Still haven't fixed downward movement
    - WIP: use cursor_y instead of copy_cursor_y
    - WIP: fix issue where $match would be unbound
    - WIP: add a new col to each line to signify file
    - WIP: concat files and refactor goto
    - WIP: format local block
    - WIP: adjust test to check for num above 1
    - WIP: Fix downward movement
       When not starting from copy-mode
    - WIP: fix column movement when doing downward
    - WIP: Fix issue with starting fuzzback in copymode
    - WIP: Remove print statements
    - WIP: remove unused function
    - WIP: move column movement to separate function
    - WIP: remove debug out
    - WIP: delete old files
    - WIP: add input fixture
    - WIP: add comments

--- scripts/fuzzback.sh
+++ scripts/fuzzback.sh
@@ -10,44 +10,40 @@ IFS=$'\n\t'
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SUPPORTED_VERSION="2.4"
 
-# https://github.com/tmux-plugins/tmux-copycat/blob/d7f7e6c1de0bc0d6915f4beea5be6a8a42045c09/scripts/helpers.sh#L12
-fuzzback::cmd_exists() {
-  command -v "$@" > /dev/null 2>&1
-}
+# shellcheck source=helpers.sh
+. "$CURRENT_DIR/helpers.sh"
 
-# Fall back to awk if gawk isn't present on system
-AWK_CMD='awk'
-if fuzzback::cmd_exists gawk; then
-  AWK_CMD='gawk'
-fi
-
-fuzzback::fzf_cmd() {
+fzf_cmd() {
   fzf-tmux --delimiter=":" \
            --ansi \
-           --with-nth="2.." \
+           --with-nth="3.." \
            --no-multi \
            --no-sort \
            --no-preview \
            --print-query
 }
 
-# Move cursor up in scrollback buffer, used when goto_line fails and we have to
-# correct
-fuzzback::cursor_up() {
+cursor_up() {
   local line_number
   line_number="$1"
   tmux send-keys -X -N "$line_number" cursor-up
 }
 
+cursor_down() {
+  local line_number
+  line_number="$1"
+  tmux send-keys -X -N "$line_number" cursor-down
+}
+
 # https://github.com/tmux-plugins/tmux-copycat/blob/d7f7e6c1de0bc0d6915f4beea5be6a8a42045c09/scripts/copycat_jump.sh#L68
-fuzzback::escape_backslash() {
+escape_backslash() {
   local string="$1"
   echo "$string" | sed 's/\\/\\\\/g'
 }
 
 # Get columns position of search query
 # https://github.com/tmux-plugins/tmux-copycat/blob/d7f7e6c1de0bc0d6915f4beea5be6a8a42045c09/scripts/copycat_jump.sh#L73
-fuzzback::query_column() {
+query_column() {
   local query="$1"
   local result_line="$2"
   local column zero_index platform
@@ -63,8 +59,8 @@ fuzzback::query_column() {
   # awk treats \r, \n, \t etc as single characters and that messes up match
   # highlighting. For that reason, we're escaping backslashes so above chars
   # are treated literally.
-  result_line="$(fuzzback::escape_backslash "$result_line")"
-  query="$(fuzzback::escape_backslash "$query")"
+  result_line="$(escape_backslash "$result_line")"
+  query="$(escape_backslash "$query")"
 
   column=$($AWK_CMD -v a="$result_line" -v b="$query" 'BEGIN{print index(a,b)}')
   zero_index=$((column - 1))
@@ -73,7 +69,7 @@ fuzzback::query_column() {
 
 # maximum line number that can be reached via tmux goto-line
 # https://github.com/tmux-plugins/tmux-copycat/blob/e95528ebaeb6300d8620c8748a686b786056f374/scripts/copycat_jump.sh#L159
-fuzzback::get_max_jump() {
+get_max_jump() {
   local max_jump max_lines window_height
   local max_lines="$1"
   local window_height="$2"
@@ -86,14 +82,59 @@ fuzzback::get_max_jump() {
 }
 
 # Goto line in scrollback buffer
-fuzzback::goto_line() {
+goto_line() {
+  local correction
   local line_number="$1"
-  tmux send-keys -X goto-line "$line_number"
+  local direction="$2"
+  local max_jump="$3"
+  correction="0"
+
+  if [ "$direction" -eq "1" ];then
+    if [ "$line_number" -gt "$max_jump" ]; then
+      # We need to reach a line number that is not accessible via goto-line.
+      # So we need to correct position to reach the desired line number
+      correct_line_number="$max_jump"
+      correction=$((line_number - correct_line_number))
+    else
+      # we can reach the desired line number via goto-line. Correction not
+      # needed.
+      correct_line_number="$line_number"
+    fi
+
+    tmux send-keys -X goto-line "$correct_line_number"
+
+    # Correct if needed
+    if [ "$correction" -gt "0" ]; then
+      cursor_up "$correction"
+    fi
+
+    # Centering
+    # -------------
+    # If no corrections (meaning result is not at the top of scrollback)
+    # we can then 'center' the result within a pane.
+    if [ "$correction" -eq "0" ]; then
+      local half_window_height="$((window_height / 2))"
+      # creating as much padding as possible, up to half pane height
+      center "$line_number" "$half_window_height"
+    fi
+  else
+    tmux send-keys -X goto-line "0"
+    cursor_down "$(( line_number + 1))"
+  fi
+
+}
+
+goto_column() {
+  local column="$1"
+  tmux send-keys -X start-of-line
+  if [ "$column" -gt "0" ]; then
+    tmux send-keys -X -N "$column" cursor-right
+  fi
 }
 
 # Center result on screen
 # https://github.com/tmux-plugins/tmux-copycat/blob/e95528ebaeb6300d8620c8748a686b786056f374/scripts/copycat_jump.sh#L127
-fuzzback::center() {
+center() {
   local number_of_lines="$1"
   local maximum_padding="$2"
   local padding
@@ -115,82 +156,121 @@ fuzzback::center() {
   tmux send-keys -X -N "$padding" cursor-up
 }
 
-fuzzback::get_line_number() {
+get_line_number() {
   local position line_number
-  position=$(echo "$1" | cut -d':' -f1 | xargs)
+  position=$(echo "$1" | cut -d':' -f2 | xargs)
   line_number=$((position - 1))
   echo "$line_number"
 }
 
+# Direction to move is stored in results at pos 1
+get_direction() {
+  local direction
+  direction=$(echo "$1" | cut -d':' -f1 | xargs)
+  echo "$direction"
+}
+
+# Get the cursor y position when starting fuzzback
+get_pos() {
+  local cursor_y
+  cursor_y=$(tmux display -p '#{cursor_y}')
+  echo "$cursor_y"
+}
+
+# Store captured scrollback in temp file
+create_capture_file() {
+  local capture_filename
+	capture_filename="$(get_capture_filename)"
+	mkdir -p "$(get_tmp_dir)"
+	chmod 0700 "$(get_tmp_dir)"
+  tmux capture-pane -p -S - > "$capture_filename"
+}
+
+# Create a file that is the head of the scrollback. Ends where the cursor y
+# position was when fuzzback was started
+create_head_file() {
+  local head_n="$1"
+  local head_filename capture_filename
+  head_filename="$(get_head_filename)"
+	capture_filename="$(get_capture_filename)"
+  head -n "$head_n" < "$capture_filename" \
+    | tac \
+    | nl -b 'a' -s ':' \
+    | sed 's/^/1:/' \
+    > "$head_filename"
+}
+
+# Create a file that is the content below cursor then starting fuzzback
+create_tail_file() {
+  local tail_n="$1"
+  local tail_filename capture_filename
+  tail_filename="$(get_tail_filename)"
+	capture_filename="$(get_capture_filename)"
+  tail -n "$tail_n" < "$capture_filename" \
+    | nl -b 'a' -s ':' \
+    | tac \
+    | sed 's/^/-1:/' \
+    > "$tail_filename"
+}
+
 fuzzback() {
+  local match line_number window_height query max_lines max_jump
+  local correct_line_number trimmed_line column pos pos_rev
+  local capture_height head_n tail_n
 
-  local content match line_number window_height query max_lines max_jump
-  local correction correct_line_number trimmed_line column
+  create_capture_file
 
-  content="$(tmux capture-pane -e -p -S -)"
-  match=$(echo "$content" | tac | nl -b 'a' -s ':' | fuzzback::fzf_cmd)
+  pos=$(get_pos)
+  window_height="$(tmux display-message -p '#{pane_height}')"
+  pos_rev=$(( window_height - pos ))
+  capture_file=$(get_capture_filename)
+  head_file=$(get_head_filename)
+  tail_file=$(get_tail_filename)
+  capture_height=$(wc -l < "$capture_file")
+  head_n=$(( capture_height - pos_rev + 1 ))
+  tail_n=$(( pos_rev - 1 ))
 
-  if [ -n "$match" ]; then
+  # We need to create two separate files to account for upward and downward
+  # movement when starting fuzzback, scrollback movement is relative to start
+  # cursor_y position
+  create_head_file "$head_n"
+  create_tail_file "$tail_n"
+
+  # Combine head and tail when searching with fzf
+  match=$(cat "$tail_file" "$head_file" | fzf_cmd)
+
+  if [ "$(echo "$match" | wc -l)" -gt "1" ]; then
     readarray -t match <<< "$match"
     query="${match[0]}"
     rest="${match[1]}"
-    trimmed_line=$(echo "$rest" | sed 's/[[:space:]]\+[[:digit:]]\+://')
-    line_number=$(fuzzback::get_line_number "$rest")
-    window_height="$(tmux display-message -p '#{pane_height}')"
-    max_lines=$(echo "$content" | wc -l)
-    max_jump=$(fuzzback::get_max_jump "$max_lines" "$window_height")
-    correction="0"
-    column=$(fuzzback::query_column "$query" "$trimmed_line")
+    trimmed_line=$(echo "$rest" | sed 's/-\?[[:digit:]]\+:[[:space:]]\+[[:digit:]]\+://')
+    line_number=$(get_line_number "$rest")
+    direction=$(get_direction "$rest")
+    column=$(query_column "$query" "$trimmed_line")
 
-    # To go line
-    # -----------------
-    if [ "$line_number" -gt "$max_jump" ]; then
-      # We need to reach a line number that is not accessible via goto-line.
-      # So we need to correct position to reach the desired line number
-      correct_line_number="$max_jump"
-      correction=$((line_number - correct_line_number))
-    else
-      # we can reach the desired line number via goto-line. Correction not
-      # needed.
-      correct_line_number="$line_number"
-    fi
+    max_lines=$(wc -l < "$head_file")
+    max_jump=$(get_max_jump "$max_lines" "$window_height")
+
+    # Quit copymode before starting a new fuzzback session This solves issues
+    # with starting fuzzback when already in copy-mode
+    tmux copy-mode -q
 
     tmux copy-mode
-    fuzzback::goto_line "$correct_line_number"
 
-    # Correct if needed
-    if [ "$correction" -gt "0" ]; then
-      fuzzback::cursor_up "$correction"
-    fi
-
-    # Set cursor to start of line when all vertical movement is done
-    tmux send-keys -X start-of-line
-
-    # Centering
-    # -------------
-    # If no corrections (meaning result is not at the top of scrollback)
-    # we can then 'center' the result within a pane.
-    if [ "$correction" -eq "0" ]; then
-      local half_window_height="$((window_height / 2))"
-      # creating as much padding as possible, up to half pane height
-      fuzzback::center "$line_number" "$half_window_height"
-    fi
-
-    # Move to column
-    # ------------------
-    if [ "$column" -gt "0" ]; then
-      tmux send-keys -X -N "$column" cursor-right
-    fi
+    # Move to position
+    goto_line "$line_number" "$direction" "$max_jump"
+    goto_column "$column"
 
   fi
+  delete_old_files
 }
 
-fuzzback::version_ok() {
+version_ok() {
   "$CURRENT_DIR/supported.sh" "$SUPPORTED_VERSION"
 }
 
 main() {
-  if fuzzback::version_ok; then
+  if version_ok; then
     fuzzback
   fi
 }
---
+++ scripts/helpers.sh
@@ -0,0 +1,42 @@
+
+# https://github.com/tmux-plugins/tmux-copycat/blob/d7f7e6c1de0bc0d6915f4beea5be6a8a42045c09/scripts/helpers.sh#L12
+cmd_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
+
+# Fall back to awk if gawk isn't present on system
+AWK_CMD='awk'
+if cmd_exists gawk; then
+  AWK_CMD='gawk'
+fi
+
+# returns a string unique to current pane
+# sed removes `$` sign because `session_id` contains is
+_pane_unique_id() {
+	tmux display-message -p "#{session_id}-#{window_index}-#{pane_index}" |
+		sed 's/\$//'
+}
+
+get_tmp_dir() {
+	echo "${TMPDIR:-/tmp}/tmux-$EUID-fuzzback"
+}
+
+get_capture_filename() {
+	echo "$(get_tmp_dir)/capture-$(_pane_unique_id)"
+}
+
+get_tail_filename() {
+	echo "$(get_tmp_dir)/tail-$(_pane_unique_id)"
+}
+
+get_head_filename() {
+	echo "$(get_tmp_dir)/head-$(_pane_unique_id)"
+}
+
+delete_old_files() {
+	local c h t
+	c="$(get_capture_filename)"
+	h="$(get_head_filename)"
+	t="$(get_tail_filename)"
+	rm -f "$c" "$h" "$t"
+}
